a17d98c3c6c212519112ba7132f9a9c86d31f2a4
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-06-19 14:48:36 +0200

    Adding testing reports

--- javascript/runCI.sh
+++ javascript/runCI.sh
@@ -4,9 +4,9 @@ currentDir=$( pwd )
 websiteUrl='http://getbootstrap.com'
 
 cd shoppingcart/galen-tests
-galen test . -DwebsiteUrl=${websiteUrl} --htmlreport ../../../reports/shopping-cart
+galen test . -DwebsiteUrl=${websiteUrl} --htmlreport ../../../reports/shopping-cart --testngreport ../../../reports/shopping-cart/testng.xml
 cd ../..
 
 cd bootstrap
-galen test basic.test -DwebsiteUrl=${websiteUrl} --htmlreport ../../reports/bootstrap/local -Dwebdriver.chrome.driver=/opt/dev/tools/chromedriver
-galen test saucelabs.test -DwebsiteUrl=${websiteUrl} --htmlreport ../../reports/bootstrap/saucelabs
+galen test basic.test -DwebsiteUrl=${websiteUrl} --htmlreport ../../reports/bootstrap/local --testngreport ../../../reports/bootstrap/local/testng.xml -Dwebdriver.chrome.driver=/opt/dev/tools/chromedriver
+galen test saucelabs.test -DwebsiteUrl=${websiteUrl} --htmlreport ../../reports/bootstrap/saucelabs --testngreport ../../../reports/bootstrap/saucelabs/testng.xml

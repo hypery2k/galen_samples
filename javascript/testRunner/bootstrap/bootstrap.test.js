35f6234fd6dcc1d54b16ca7a93a4fc009724ac35
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-11-30 17:04:46 +0100

    Update specs

--- javascript/testRunner/bootstrap/bootstrap.test.js
+++ javascript/testRunner/bootstrap/bootstrap.test.js
@@ -3,14 +3,14 @@ load("pages/homePage.js");
 
 testOnAllDevices("Bootstrap homepage", "/", function (driver, device) {
     var homePage = new HomePage(driver).waitForIt();
-    checkLayout(driver, "specs/homePageLayout.spec", device.tags);
+    checkLayout(driver, "specs/homePageLayout.gspec", device.tags);
     homePage.goToExpoButton().click();
 });
 
 testOnDeviceAndBrowsers(devices.mobile, "Bootstrap CSS page", "/css/", function (driver, device, browser) {
-    checkLayout(driver, "specs/cssPageLayout.spec", device.tags);
+    checkLayout(driver, "specs/cssPageLayout.gspec", device.tags);
 });
 
 testOnAllDevicesAndBrowsers("Bootstrap JS page ", "/javascript/", function (driver, device, browser) {
-    checkLayout(driver, "specs/javascriptPageLayout.spec", device.tags);
+    checkLayout(driver, "specs/javascriptPageLayout.gspec", device.tags);
 });
--- javascript/testSuite/bootstrap/specs/shared/common.spec
+++
@@ -1,8 +0,0 @@
-
-@objects
-  navbar			.navbar-header
-  navbar-item-*		.navbar-collapse .nav li 
-  menubar-left		.sidebar-left
-  header			.bs-docs-header
-  content			.bs-docs-container
-  header-container	.bs-docs-header .container
